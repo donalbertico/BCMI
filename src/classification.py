47527f94aed44f4d76a87cb09671a2b680e71607
donalbertico <albertotates@hotmail.es>
2019-08-27 13:18:44 +0100

    adding comments to code

--- src/classification.py
+++ src/classification.py
@@ -1,6 +1,7 @@
 from preprocessing import getEpochs
 from preprocessing import getRaw
 from preprocessing import getAvarage
+from preprocessing import getEpochsICA
 from sklearn import svm
 from sklearn.pipeline import Pipeline
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -8,20 +9,41 @@ from sklearn.model_selection import ShuffleSplit, cross_val_score
 from mne.decoding import CSP
 import numpy as np
 
-
-def classify(experiments, components, task):
-    epochs = getEpochs(experiments,task)
+# Print score of certain arrangement
+#
+# Trains and apply 10 fold cross validation over the preprocessed signal
+# experiments - number of experiments (1,2,3)
+# components for csp
+# tasks signal classes ['M consonant','break','Ye sound','M + movement']
+# classifier - (lda,svm)
+# ica - apply ica, Boolean
+#
+#
+def score(experiments, components, tasks, classifier ,ica):
+    if(ica):
+        epochs = getEpochsICA(experiments,tasks)
+    else:
+        epochs = getEpochsICA(experiments,tasks)
     print(epochs.__len__)
     cv = ShuffleSplit(10,test_size=0.2, random_state =42)
     epochs_data = epochs.get_data()
-    epochs.average().plot(spatial_colors=True,time_unit='s')
     y = epochs.events[:,2]
-    lda = LinearDiscriminantAnalysis()
     csp = CSP(n_components=components, reg=None,log=False ,norm_trace = False)
-    clf = Pipeline([('CSP',csp),('LDA',lda)])
+    if(classifier == 'lda'):
+        lda = LinearDiscriminantAnalysis()
+        clf = Pipeline([('CSP',csp),('LDA',lda)])
+    else:
+        svm = LinearDiscriminantAnalysis()
+        clf = Pipeline([('CSP',csp),('SVM',lda)])
     scores = cross_val_score(clf,epochs_data,y, cv=cv, n_jobs=1)
-    print('mean accuracy tasks :', task  ,' ',np.mean(scores))
+    print('mean accuracy : ica ',ica ,',classifier', classifier,',tasks ', tasks,' ',np.mean(scores))
 
+# Plots CSP components
+#
+# experiments - number of experiments (1,2,3)
+# components - for csp
+# tasks - signal classes ['M consonant','break','Ye sound','M + movement']
+#
 def plotCSP(experiments,components,task):
     epochs = getEpochs(experiments,task)
     y = epochs.events[:,2]
@@ -30,18 +52,28 @@ def plotCSP(experiments,components,task):
     csp.fit_transform(epochs_data, y)
     csp.plot_patterns(epochs.info,ch_type='eeg', units = 'Patterns (AU)', size = 1.5)
 
+# Plots raw data for first experiment record
+#
+#
 def visualizeRaw():
-    raw,events,event_id,event_color = getRaw(1)
+    raw,events,event_id,event_color = getRaw()
     raw.plot( duration = 10,events = events, event_id = event_id, event_color = event_color, scalings = {'eeg':75e-6}, block= True)
 
-def visualizeEpochs(tasks):
+# Plots epoch mne object with the events marked
+#
+# experiments - number of experiments (1,2,3)
+# tasks - signal classes ['M consonant','break','Ye sound','M + movement']
+#
+def visualizeEpochs(experiments,tasks):
+    event_color = {1:'peachpuff', 2: 'c', 3:'peachpuff', 7:'peachpuff'}
     epochs = getEpochs(1,tasks)
-    # epochs = getAvarage(tasks)
-    # epochs.plot(spatial_colors=True,time_unit='s')
     epochs.plot(n_epochs = 2, event_colors = event_color, scalings = {'eeg':75e-6}, block= True)
 
-# visualizeEpochs([])
-plotCSP(1,10,['M consonant','Ye sound','break'])
-# visualizeRaw()
-
-# visualizeEpochs(['M consonant'])
+# get Epochs object with ica applied
+#
+# experiments - number of experiments (1,2,3)
+# tasks - signal classes ['M consonant','break','Ye sound','M + movement']
+#
+def icaEpochs(experiments,tasks):
+    epochs = getEpochsICA(experiments,tasks)
+    return epochs
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,13 +2,16 @@ import mne
 import numpy as np
 from mne.channels import read_layout
 
-# ch_dic = np.loadtxt('ch_dic.csv',delimiter = ',', dtype=str)
-ch_dic = np.loadtxt('src/ch_dic.csv',delimiter = ',', dtype=str)
+ch_dic = np.loadtxt('ch_dic.csv',delimiter = ',', dtype=str)
 ch_dic = dict(zip(ch_dic[0],ch_dic[1]))
 event_id = {'M consonant': 1, 'Ye sound': 3, 'M + movement' : 7, 'break' : 2 }
 event_color = {1:'peachpuff', 2: 'c', 3:'peachpuff', 7:'peachpuff'}
 montage = mne.channels.read_montage('biosemi64')
 
+# Filter the unknown events
+#
+#events from stim channel
+#
 def reduceEvents(events):
     events[:,2] += -(2**16-255)
     newEvents = []
@@ -41,21 +44,27 @@ def reduceEvents(events):
     newEvents = np.stack(newEvents)
     return newEvents
 
+
+# Return epochs object after apply bandpass (7,30)
+#
+# experiments - number of experiments (1,2,3)
+# components - for csp
+#
 def getEpochs(experiments,tasks):
     if(experiments >= 1):
-        raw = mne.io.read_raw_bdf("data/e01.bdf")
+        raw = mne.io.read_raw_bdf("../data/e01.bdf")
         events = mne.find_events(raw, shortest_event = 1)
         events = reduceEvents(events)
         picks = mne.pick_types(raw.info,eeg = True,stim =True ,exclude = 'bads')
         epochs = mne.Epochs(raw, events, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None, preload = True)
     if(experiments >= 2):
-        raw_02 = mne.io.read_raw_bdf("data/e02.bdf")
+        raw_02 = mne.io.read_raw_bdf("../data/e02.bdf")
         events2 = mne.find_events(raw_02, shortest_event = 1)
         events2 = reduceEvents(events2)
         epochs2 = mne.Epochs(raw_02, events2, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None)
         epochs = mne.concatenate_epochs([epochs,epochs2])
     if(experiments >= 3):
-        raw_03 = mne.io.read_raw_bdf("data/e03.bdf")
+        raw_03 = mne.io.read_raw_bdf("../data/e03.bdf")
         events3 = mne.find_events(raw_03, shortest_event = 1)
         events3 = reduceEvents(events3)
         epochs3 = mne.Epochs(raw_03, events3, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None)
@@ -70,58 +79,66 @@ def getEpochs(experiments,tasks):
     else :
         return epochs
 
+# Return epochs object after apply ICA and bandapass (1,60)
+#
+# experiments - number of experiments (1,2,3)
+# components - for csp
+#
 def getEpochsICA(experiments,tasks):
+    ica = mne.preprocessing.ICA(method="fastica",random_state=23)
     if(experiments >= 1):
-        raw = mne.io.read_raw_bdf("data/e01.bdf", preload = True)
-        raw.rename_channels(ch_dic)
-        raw.set_montage(montage)
+        raw = mne.io.read_raw_bdf("../data/e01.bdf", preload = True)
+        picks = mne.pick_types(raw.info,eeg = True,stim =True ,exclude = 'bads')
         events = mne.find_events(raw, shortest_event = 1)
         events = reduceEvents(events)
-        picks = mne.pick_types(raw.info,eeg = True,stim =True ,exclude = 'bads')
-        raw.filter(1., None, n_jobs=1, fir_design='firwin')
-        # raw.notch_filter(np.arange(50, 241, 50), picks=picks, fir_design='firwin')
-        ica = mne.preprocessing.ICA(method="fastica",random_state=23,n_components = 35)
-        ica.fit(raw,decim=3)
-        ica.plot_components(inst = raw)
-        ica.exclude = [2]
-        ica.apply(raw)
-        # raw.filter(7, 30, n_jobs=1, fir_design='firwin')
-        # epochs = mne.Epochs(raw, events, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None, preload = True)
-        # raw.filter(7.,30.,fir_design = 'firwin')
+        raw.notch_filter(np.arange(50, 250, 50), picks=picks, fir_design='firwin')
         epochs = mne.Epochs(raw, events, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None)
     if(experiments >= 2):
-        raw_02 = mne.io.read_raw_bdf("data/e02.bdf")
+        raw_02 = mne.io.read_raw_bdf("../data/e02.bdf", preload = True)
+        picks = mne.pick_types(raw_02.info,eeg = True,stim =True ,exclude = 'bads')
         events2 = mne.find_events(raw_02, shortest_event = 1)
         events2 = reduceEvents(events2)
+        raw_02.notch_filter(np.arange(50, 250, 50), picks=picks, fir_design='firwin')
         epochs2 = mne.Epochs(raw_02, events2, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None)
         epochs = mne.concatenate_epochs([epochs,epochs2])
     if(experiments >= 3):
-        raw_03 = mne.io.read_raw_bdf("data/e03.bdf")
+        raw_03 = mne.io.read_raw_bdf("../data/e03.bdf", preload = True)
+        picks = mne.pick_types(raw_03.info,eeg = True,stim =True ,exclude = 'bads')
         events3 = mne.find_events(raw_03, shortest_event = 1)
         events3 = reduceEvents(events3)
+        raw_03.notch_filter(np.arange(50, 250, 50), picks=picks, fir_design='firwin')
         epochs3 = mne.Epochs(raw_03, events3, event_id, tmin = 0.1, tmax = 5, proj=True, picks=picks, baseline = None)
         epochs = mne.concatenate_epochs([epochs,epochs3])
 
-    # epochs.rename_channels(ch_dic)
+    epochs.rename_channels(ch_dic)
     epochs.set_montage(montage)
-    # epochs.filter(7.,30.,fir_design='firwin')
-
+    epochs.filter(1., 60, n_jobs=1, fir_design='firwin')
+    ica.fit(epochs['M consonant','break'],decim=3)
+    ica.plot_components()
+    ica.exclude = [48]
+    ica.apply(epochs)
+    epochs.plot(n_epochs = 2, event_colors = event_color, scalings = {'eeg':75e-6}, block= True)
     if(tasks):
         return epochs[tasks]
     else :
         return epochs
 
-
-def getRaw(experiments):
-    raw = mne.io.read_raw_bdf("data/e01.bdf",preload = True)
+# Return raw data from first exeriment record
+#
+#
+def getRaw():
+    raw = mne.io.read_raw_bdf("../data/e01.bdf",preload = True)
+    picks = mne.pick_types(raw.info,eeg = True,stim =True ,exclude = 'bads')
     events = mne.find_events(raw, shortest_event = 1)
     events = reduceEvents(events)
     raw.rename_channels(ch_dic)
     raw.set_montage(montage)
     raw.notch_filter(np.arange(50, 250, 50), picks=picks, fir_design='firwin')
-    # raw.filter(7.,30., fir_design='firwin')
     return raw,events,event_id,event_color
 
+# Plots ICA compoents from frist recrods
+#
+#
 def plotICA():
     raw = mne.io.read_raw_bdf("../data/e01.bdf",preload = True)
     events = mne.find_events(raw, shortest_event = 1)
@@ -138,25 +155,3 @@ def plotICA():
     ica.exclude = [2]
     ica.apply(raw)
     raw.plot_psd(tmin=93, tmax=98, fmax= 200, picks = picks)
-
-def getAvarage(tasks):
-    reject = dict(eeg = 80e-6)
-    raw = mne.io.read_raw_bdf("data/e01.bdf",preload = True)
-    raw.rename_channels(ch_dic)
-    raw.set_montage(montage)
-    picks = mne.pick_types(raw.info,eeg = True,stim =True ,exclude = 'bads')
-    events = mne.find_events(raw, shortest_event = 1)
-    events = reduceEvents(events)
-    raw.filter(1., None, n_jobs=1, fir_design='firwin')
-    # raw.notch_filter(np.arange(50, 241, 50), picks=picks, fir_design='firwin')
-    # raw.filter(None, 150., fir_design='firwin')
-    ica = mne.preprocessing.ICA(method="fastica",random_state=23,n_components = 15)
-    ica.fit(raw,decim=3)
-    # ica.plot_components(inst = raw)
-    ica.exclude[2]
-    ica.apply(raw)
-    epochs = mne.Epochs(raw, events, event_id, tmin = -0.1 ,tmax = 2,proj=True ,picks=picks, baseline = (None,0))
-
-    print(epochs.__len__)
-    # epochs.filter(7.,30.,fir_design='firwin')
-    return epochs[tasks].average()
